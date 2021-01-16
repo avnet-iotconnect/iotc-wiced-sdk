4c6e98a0227e12e44f163ca4331eaeac57a920c9
Nik Markovic <nik.markovic@vensi.com>
2021-01-15 18:22:31 -0600

    remove mqtt code as it's no longer needed with working discovery

--- 43xxx_Wi-Fi/apps/demo/iotconnect_demo/iotconnect_mqtt_client.h
+++ 43xxx_Wi-Fi/apps/demo/iotconnect_demo/iotconnect_mqtt_client.h
@@ -7,15 +7,6 @@ extern "C" {
 
 #include "wiced.h"
 
-const char *mqtt_get_host(const char *env);
-
-char * mqtt_create_client_id_str(const char* cpid, const char* duid);
-
-char *mqtt_create_pub_topic_str(const char *clientid);
-
-char *mqtt_create_sub_topic_str(const char *clientid);
-
-char *mqtt_create_username_str(const char *clientid, const char *host);
 
 #ifdef __cplusplus
 } /* extern "C" */
