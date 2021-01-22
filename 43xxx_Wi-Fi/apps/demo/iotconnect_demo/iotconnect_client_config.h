b21171bc8baf32812c31fc3e7833b2e05fdb0c81
Nik Markovic <nik.markovic@vensi.com>
2021-01-22 16:50:17 -0600

    Reduce client_config.h to not include no longer needed values

--- 43xxx_Wi-Fi/apps/demo/iotconnect_demo/iotconnect_client_config.h
+++ 43xxx_Wi-Fi/apps/demo/iotconnect_demo/iotconnect_client_config.h
@@ -5,9 +5,7 @@ extern "C" {
 #endif
 
 #define IOTCONNECT_CPID                     "YOURCPID"
-#define IOTCONNECT_DUID                     "YOURDEVICEID"
 #define IOTCONNECT_ENV                      "YOURENV"
-#define IOTCONNECT_DTG                      "YOURDTG" /* TODO: Support http sync response */
 
 #define API_VERSION_STRING                  "api-version=2018-06-30"
 
